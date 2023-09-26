78f1fe3ae11e5c6793f8017292f6b6ca5aaa973d
Juan Grajales <juan.grajales001@gmail.com>
2023-09-26 12:26:04 -0700

    #23 refactored s3 bucket into the new terrahouse module (#24)

--- variables.tf
+++ variables.tf
@@ -1,17 +1,7 @@
-variable "user_uuid" {
-  type        = string
-  description = "The UUID of the user"
-  validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]", var.user_uuid))
-    error_message = "The user_uuid value must be a valid."
-  }
+variable "user_uuid"{
+    type = string
 }
 
 variable "bucket_name" {
-  description = "Name for the S3 bucket"
-  type        = string
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9.-]+$", var.bucket_name))
-    error_message = "Bucket name must only contain letters, numbers, hyphens, and periods."
-  }
+    type = string
 }
