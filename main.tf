d92d34f2860afe78b1b36bb14fa99686102e336c
Juan Grajales <juan.grajales001@gmail.com>
2023-09-26 18:45:56 +0000

    #21 documentation on Terraform import, added variable name bucket_name for s3, remove random provider

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-resource "random_string" "bucket_name" {
-  # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-  length = 32
-  special = false
-  upper = false
-  lower = true
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
   tags = {
     UserUuid = var.user_uuid
   }
